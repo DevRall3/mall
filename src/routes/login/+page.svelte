cc48821b3e1f92a7055624611283256ff6b761f9
Rasmus <rasmik@edu.umea.se>
2024-04-09 10:26:32 +0200

    indeed

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+    import type { ActionData } from "./$types.js";
+
+    export let form: ActionData;
+    // export let data;
+</script>
+
+<!-- Enhance enhancear formuläret -->
+<body>
+    <div class="login-container">
+        <div class="login-div">
+            <h1>Login</h1>
+            <form method="post" action="?/login" use:enhance>
+                <label>Email: <input type="email" name="email" /></label>
+                <label
+                    >Password: <input type="password" name="password" /></label
+                >
+
+                <button class="main-btn"
+                    ><span class="text">Log In</span></button
+                >
+                {#if form?.userName}
+                    <span style="color: red;">{form.userName}</span>
+                {/if}
+            </form>
+            <span>Don't have an account? </span>
+            <a href="/register"
+                >Register here</a
+            >
+        </div>
+    </div>
+</body>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { enhance } from '$app/forms';
+    import type { ActionData } from './$types';
+    
+    export let form: ActionData;
+    // export let data: PageData;
+</script>
+<div class="register-container">
+    <div class="register-div">
+        <h1>Register account</h1>
+        <form action="?/register" method="post" use:enhance>
+            <label>E-mail: <input type="email" name="email"></label>
+            <label>Name: <input type="text" name="fullName"></label>
+            <label>Password: <input type="password" name="password"></label>
+            <label>Repeat Password: <input type="password" name="password-repeat"></label>
+            <button class="main-btn"><span class="text">Submit</span></button>
+            {#if form?.msg}
+                <span style="color: red;">{form.msg}</span>
+            {/if}
+        </form>
+        <span>Already have an account? </span><a href="/login">Log in here</a>
+    </div>
+</div>
